a7ef044ae7a3fcce1b24fddc020fc236ba76cd3d
Shantanu Jaiswal <Shantanu.9.Jaiswal>
2026-01-01 17:39:51 +0530

    splash done

--- composeApp/src/commonMain/kotlin/com/business/cmpproject/presentation/features/splash/SplashScreen.kt
+++ composeApp/src/commonMain/kotlin/com/business/cmpproject/presentation/features/splash/SplashScreen.kt
@@ -1,17 +1,9 @@
 package com.business.cmpproject.presentation.features.splash
 
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import kotlinx.coroutines.delay
-
-import androidx.compose.runtime.*
-import kotlinx.coroutines.delay
-import androidx.compose.material3.Text
 import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.koin.getScreenModel
 import cafe.adriel.voyager.navigator.LocalNavigator
-import cafe.adriel.voyager.navigator.Navigator
 import com.business.cmpproject.presentation.features.dashboard.DashboardScreen
 import com.business.cmpproject.presentation.features.login.LoginScreen
 
